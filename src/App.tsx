3bca1f9e3c698edff6b0f59ec47a0b914ca1e237
DeLesslin George-Warren <delesslingw@gmail.com>
2025-01-22 22:04:25 -0500

    testing if portfolio update will update web server (it should not)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
         <>
             <h1>Tanake Manu. Ni yat DeLesslin</h1>
             <h2>Hello world. My name is DeLesslin</h2>
+            <h3>This should not update on the live server</h3>
         </>
     );
 }
