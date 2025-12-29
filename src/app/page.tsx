478fadcbe60be65b3308991cb371edd1285e847a
delesslin <delesslingw@gmail.com>
2025-12-29 00:55:05 -0500

    Remove subtitle and adjust header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
-import P5Canvas from "@/components/P5Canvas";
-import Projects from "../components/Projects";
-import MotionTitle from "@/components/MotionTitle";
-export const revalidate = false; // SSG at build; set to a number for ISR
+import MotionTitle from '@/components/MotionTitle'
+import P5Canvas from '@/components/P5Canvas'
+import Projects from '../components/Projects'
+export const revalidate = false // SSG at build; set to a number for ISR
 
 export default function Home() {
-    return (
-        <>
-            <header className="relative overflow-hidden grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <MotionTitle />
-                <div className="absolute inset-0 -z-10">
-                    <P5Canvas />
-                </div>
-                <h1 style={{ zIndex: 100 }}>Like life, a work in progress. Check back soon...</h1>
-            </header>
-            <main>
-                <Projects />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <header className='relative overflow-hidden grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+        <MotionTitle />
+        <div className='absolute inset-0 -z-10'>
+          <P5Canvas />
+        </div>
+      </header>
+      <main>
+        <Projects />
+      </main>
+    </>
+  )
 }
