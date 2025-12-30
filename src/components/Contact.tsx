b5a745cb0dd9d86379e4ba94fb3c6c47bb6f5d65
delesslin <delesslingw@gmail.com>
2025-12-29 22:26:07 -0500

    Style contact email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,8 +7,8 @@ const Contact = () => {
       style={{ borderColor: color }}
     >
       <div className='mx-4 flex-1 p-4' style={{ backgroundColor: color }}>
-        <h3 className='text-2xl text-white font-bold opacity-90'>Contact</h3>
-        <h4 className='text-white opacity-80 text-lg'>
+        <h3 className='text-2xl text-white font-bold'>Contact</h3>
+        <h4 className='text-white text-lg mb-4'>
           Have a question, idea, or project you want to discuss?
         </h4>
         <ContactForm />
