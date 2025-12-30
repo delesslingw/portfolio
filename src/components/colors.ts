762e40f2e6961f2cc37da1b3163b25cc3b1f9ca8
delesslin <delesslingw@gmail.com>
2025-12-29 22:38:10 -0500

    Add random color to emails

---
+++ src/components/colors.ts
@@ -0,0 +1,17 @@
+const colors = [
+  '#fe9aaa',
+  '#fea741',
+  '#d19d3a',
+  '#f7f58c',
+  '#c1fc40',
+  '#2dfe50',
+  '#34feaf',
+  '#65ffea',
+  '#90c7fc',
+  '#5c86e1',
+  '#97abfb',
+  '#d975e2',
+  '#e55ca2',
+]
+
+export default colors
