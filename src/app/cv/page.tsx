76543af5a96e1a09138b2f4d272024730477cb10
delesslin <delesslingw@gmail.com>
2026-03-13 11:57:43 -0500

    Add nav links to homepage

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,8 @@
+export default function CV() {
+  return (
+    <main className='min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <h1 className='text-7xl font-black'>CV</h1>
+      <p className='mt-2 italic text-neutral-700'>Coming soon.</p>
+    </main>
+  )
+}
--- src/components/MotionTitle.tsx
+++ src/components/MotionTitle.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 
 export default function MotionTitle() {
   return (
     <motion.h1
       className='absolute top-4 left-4 text-4xl sm:text-4xl lg:text-4xl font-mono text-neutral-700 opacity-70 z-99'
-      initial={{ x: 48, opacity: 0 }} // start slightly to the right
-      animate={{ x: 0, opacity: 1 }} // move into place + fade in
+      initial={{ x: 48, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
       transition={{ delay: 1, duration: 1.5, ease: 'easeOut' }}
     >
-      DELESSLIN
+      <Link href='/'>DELESSLIN</Link>
     </motion.h1>
   )
 }
